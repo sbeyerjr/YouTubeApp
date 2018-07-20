738297f0586d75049734cb30bdc816b585a0fc1d
Steven Beyer <sbeyerjr@gmail.com>
2018-07-19 20:13:48 -0700

    added more videos button

--- main.js
+++ main.js
@@ -1,24 +1,7 @@
 const YOUTUBE_SEARCH_URL = 'https://www.googleapis.com/youtube/v3/search';
 const YOUTUBE_URL = 'https://www.youtube.com/watch?v=';
-// function getDataFromApi(searchTerm, callback){
-// 	const query = {
-// 		q: `${searchTerm} in:name`,
-// 		part: 'snippet',
-// 		key: 'AIzaSyCkTGJ70KnO7SnMU5D5MguVWXN21HgCTQY',
-// 	}
-// 	$.getJSON(YOUTUBE_SEARCH_URL, query, callback);
-// }
+const YOUTUBE_CHANNEL_URL = 'https://www.youtube.com/channel/';
 
-// function renderResult(result) {
-//   return `
-//     <div>
-//       <h2>
-//       <a class="js-result-name" href="${result.url}" target="_blank">${result.title}</a> by <span class="js-user-name">${result.channelTitle}</span></h2>
-//       <p class="vid-description">${result.description}</p>
-      
-//     </div>
-//   `;
-// }
 
 function getDataFromApi(searchTerm, callback) {
   const query = {
@@ -31,19 +14,6 @@ function getDataFromApi(searchTerm, callback) {
   return console.log(searchTerm);
 }
 
-// function renderResult(result) {
-// 	let videoId = result.id.videoId;
-//   return `<div class="col-4">
-//   <img src='${result.snippet.thumbnails.medium.url}' alt=''></div>
-//   			<div class="col-8">
-//   			<div class="result-box">
-//   					<div class="title"><h2>${result.snippet.title}</h2></div>
-//   					<a href='${YOUTUBE_URL}${videoId}' target='_blank'>
-//   					</a>
-//   			</div>
-//   			</div>`;
-// }
-
 function renderResult(result) {
 	let videoId = result.id.videoId;
   return `<div class="row result-box">
@@ -54,7 +24,7 @@ function renderResult(result) {
   				<div class="title"><h2>${result.snippet.title}</h2></div>
   				<div class="description-box">${result.snippet.description}</div>
   				<div class="watch">
-  				<a class="watch-button" href="${YOUTUBE_URL}${videoId}" data-lity>Watch Video</a>
+  				<a class="watch-button" href="${YOUTUBE_URL}${videoId}" data-lity>Watch Video</a><a class="watch-button" href="${YOUTUBE_CHANNEL_URL}${result.snippet.channelId}" >More Videos By This Channel</a>
   				</div>
   			</div>
   		</div>
@@ -69,15 +39,15 @@ function displayYouTubeSearchData(data) {
 function watchSubmitButton() {
   $('.js-search-form').submit(event => {
     event.preventDefault();
+    $('.more-button').removeClass('hidden');
     const queryTarget = $(event.currentTarget).find('.js-query');
     const query = queryTarget.val();
     // clear out the input
     queryTarget.val("");
     getDataFromApi(query, displayYouTubeSearchData);
-
+    moreButton();
   });
 }
-
 
 
 $(watchSubmitButton);
